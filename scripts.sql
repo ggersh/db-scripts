68e600ff554a3a610bdf1103160b36fc2bb42248
Greg Gershkowitz <gershkowitz.3@osu.edu>
2019-04-09 22:30:18 -0400

    init commit

---
+++ scripts.sql
@@ -0,0 +1,116 @@
+CREATE TABLE Branch (
+
+);
+CREATE TABLE Department (
+
+);
+CREATE TABLE Office (
+
+);
+CREATE TABLE Factory (
+
+);
+CREATE TABLE Warehouse (
+
+);
+CREATE TABLE Sales (
+
+);
+--
+CREATE TABLE Customer (
+
+);
+CREATE TABLE Customer_address (
+
+);
+CREATE TABLE Customer_fax (
+
+);
+CREATE TABLE Customer_phone (
+
+);
+CREATE TABLE Customer_email (
+
+);
+CREATE TABLE Customer_contact (
+
+);
+--
+CREATE TABLE Manufacturer (
+
+);
+CREATE TABLE Manufacturer_address (
+
+);
+CREATE TABLE Manufacturer_fax (
+
+);
+CREATE TABLE Manufacturer_phone (
+
+);
+CREATE TABLE Manufacturer_email (
+
+);
+CREATE TABLE Manufacturer_contact (
+
+);
+--
+CREATE TABLE Part (
+
+);
+CREATE TABLE Product (
+
+);
+CREATE TABLE BoM (
+
+);
+CREATE TABLE Payment (
+
+);
+CREATE TABLE Order (
+
+);
+CREATE TABLE Routing_sheet (
+
+);
+CREATE TABLE Assembly_stage (
+
+);
+CREATE TABLE Invoice (
+
+);
+CREATE TABLE Employee (
+
+);
+CREATE TABLE Dependent (
+
+);
+--
+CREATE TABLE Office_worker (
+
+);
+CREATE TABLE Factory_worker (
+
+);
+CREATE TABLE Warehouse_worker (
+
+);
+CREATE TABLE Salesperson (
+
+);
+-- 
+CREATE TABLE OrderContainsProduct (
+
+);
+CREATE TABLE BomContainsPart (
+
+);
+CREATE TABLE ManufacturerMakesPart (
+
+);
+CREATE TABLE EmployeeWorksDepartment (
+
+);
+CREATE TABLE BranchStocksPart (
+
+);
